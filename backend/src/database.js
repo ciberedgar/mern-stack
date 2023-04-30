c67e1b934b7b6469656b11162af9740c981cb0cc
ciberedgar <edgar.rodriguez.antillon@uabc.edu.mx>
2023-04-29 17:37:55 -0700

    update2

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 const URI = process.env.MONGO_URL
-    ? process.env.MONGO_URL
-    : 'mongodb://localhost/mernstack';
 mongoose.connect(URI, {
-
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 const connection = mongoose.connection;
 connection.once('open', () => {
